b178a64c4347401c08ed60f41705a46993f8c544
邦甫 蕭 <hjacob1821@gmail.com>
2024-03-01 21:29:08 +0800

    整理file

--- files.cmake
+++ files.cmake
@@ -1,9 +1,14 @@
 set(SRC_FILES
     App.cpp
+    MenuScene.cpp
+    GameObjectEx.cpp
 )
 
 set(INCLUDE_FILES
     App.hpp
+    Scene.hpp
+    MenuScene.hpp
+    GameObjectEx.hpp
 )
 
 set(TEST_FILES
