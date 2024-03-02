46e5376e1cfcadd3a474d1259042d8c21346d1f0
邦甫 蕭 <hjacob1821@gmail.com>
2024-03-02 13:51:31 +0800

    files

--- files.cmake
+++ files.cmake
@@ -3,6 +3,8 @@ set(SRC_FILES
     MenuScene.cpp
     GameObjectEx.cpp
     GameButton.cpp
+    AnimatedGameObject.cpp
+    Utility.cpp
 )
 
 set(INCLUDE_FILES
@@ -11,6 +13,8 @@ set(INCLUDE_FILES
     MenuScene.hpp
     GameObjectEx.hpp
     GameButton.hpp
+    AnimatedGameObject.hpp
+    Utility.hpp
 )
 
 set(TEST_FILES
