0155bc1e5b0c391a0ce93600e43e04140c91c8bc
Jacob <hjacob1821@gmail.com>
2024-03-07 11:51:04 +0800

    tmp

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -2,6 +2,7 @@
 
 #include "Enemy.hpp"
 #include "Cat.hpp"
+#include "Util/Image.hpp"
 
 enum class EntityType : size_t {
     CAT = 0,
@@ -25,11 +26,16 @@ public:
 
     int GetDamage(std::optional<EnemyAttr> attr) const;
 
+    std::optional<EnemyAttr> GetEnemyAttr() const;
+
     EntityType GetEntityType() const;
 
     HitBox GetHitBox() const;
 
+    void Draw(Util::Image &image) const;
+
 private:
     EntityState m_State = EntityState::WALK;
     EntityStats m_Stats;
+    bool is_enemy = false;
 };
