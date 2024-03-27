4f16226e77a289ea3d6f77707dfadcd2c791d1e7
111590047 <t111590047@ntut.org.tw>
2024-03-27 21:47:17 +0800

    .

--- src/App.cpp
+++ src/App.cpp
@@ -61,7 +61,8 @@ void App::SwitchScene(const SceneType type) {
 }
 
 void App::SwitchToBattleScene(Stage stage) {
-    m_BattleScene->LoadStage(std::move(stage));
+    m_BattleScene->LoadStage(stage);
+    //m_BattleScene->LoadStage(std::move(stage));
     SwitchScene(SceneType::BATTLE_SCENE);
 }
 
